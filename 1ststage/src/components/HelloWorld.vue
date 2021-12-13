b73d9fd576dad4eac3ebfecf3f24d851f47261d1
GANitak <blanc-m-a@protonmail.com>
2021-12-13 14:48:00 +0100

    added files test and front page

---
+++ 1ststage/src/components/HelloWorld.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br />
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
+        >vue-cli documentation</a
+      >.
+    </p>
+    <h3>How do you feel?</h3>
+    <h5>Tell you that i love you!</h5><br>
+    <h6>...We can go on forever...</h6>
+    <ul>
+      <li>
+        <a
+          href="https://github.com/GANitak/Affair1.0.git"
+          target="_blank"
+          rel="noopener"
+          >Affair1.0</a
+        >
+      </li>
+      <li>
+        <a href="https://twitter.com/GANit_ak" target="_blank" rel="noopener"
+          >insert clickable svgIcon_Twitter</a
+        >
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
